604d29778245166ee6cb8fa859ae3ad0d27d2a5f
robinttt333 <robinttt333@gmail.com>
2018-05-23 10:58:38 +0530

    urls.py created for app persons and homepage view added

--- project/musify/persons/views.py
+++ project/musify/persons/views.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def homepage(request):
+    return HttpResponse("Hello World")
