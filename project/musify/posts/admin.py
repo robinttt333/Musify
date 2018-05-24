b6d6a1f07a4c6a80634b1ece2d906ae243949ca3
robinttt333 <robinttt333@gmail.com>
2018-05-24 09:14:27 +0530

    Creted model Post for posts app and registered it with admin

--- project/musify/posts/admin.py
+++ project/musify/posts/admin.py
@@ -4,3 +4,5 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+admin.site.register(Post)
--- project/musify/posts/models.py
+++ project/musify/posts/models.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from persons.models import person
+class Post(models.Model):
+    author=models.ForeignKey(person)
+    tags=models.CharField(max_length=150)
+    title=models.CharField(max_length=120)
+    timestamp=models.DateTimeField(auto_now_add=True)
+    lyrics=models.CharField(max_length=300,null=True)
+    artist=models.CharField(max_length=120)
+
+    def __unicode__(self):
+        return self.title+','+self.author
