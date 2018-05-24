746f792efc75d59333b515dee96ad977971f5e6f
robinttt333 <robinttt333@gmail.com>
2018-05-24 09:06:32 +0530

    Created urls.py for posts app

--- project/musify/posts/urls.py
+++ project/musify/posts/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from . import views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^',include("persons.urls",namespace="persons")),
-    url(r'^posts/',include("posts.urls",namespace="posts"))
+    url(r'',views.list,name="list"),
 ]
--- project/musify/posts/views.py
+++ project/musify/posts/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def list(request):
+    return HttpResponse("lists worked")
