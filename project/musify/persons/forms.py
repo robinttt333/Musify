93fbcf476f3cfee9a70b2aa3070de0eb99278faa
robinttt333 <robinttt333@gmail.com>
2018-05-26 12:29:31 +0530

    Added validations for signup

--- project/musify/persons/forms.py
+++ project/musify/persons/forms.py
@@ -16,11 +16,6 @@ class Login_form(forms.Form):
         self.fields['password'].widget.attrs['placeholder'] = 'Password'
 
 
-
-
-
-
-
     def clean(self,*args,**kwargs):
         username=self.cleaned_data.get('username')
         password=self.cleaned_data.get('password')
@@ -30,3 +25,28 @@ class Login_form(forms.Form):
             raise forms.ValidationError("Invalid username or password")
 
         return super(Login_form,self).clean(*args,**kwargs)
+
+
+class Registration_form(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['username','email','password']
+
+    def __init__(self, *args, **kwargs):
+        super(Registration_form, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'full-width has-padding has-border'
+        self.fields['email'].widget.attrs['class'] = 'full-width has-padding has-border'
+        self.fields['password'].widget.attrs['class'] = 'full-width has-padding has-border'
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['email'].widget.attrs['placeholder'] = 'E-mail'
+        self.fields['password'].widget.attrs['placeholder'] = 'Password'
+
+    def clean_email(self,*args,**kwargs):
+        email=self.cleaned_data.get('email')
+        instances=User.objects.filter(email=email)
+
+        if instances.count():
+            raise forms.ValidationError("A user already exists with that email")
+
+        return super(Registration_form,self).clean(*args,**kwargs)
--- project/musify/persons/views.py
+++ project/musify/persons/views.py
@@ -6,20 +6,28 @@ from django.http import HttpResponseRedirect,HttpResponse
 # Create your views here.
 from persons.models import person
 
-from .forms import Login_form
+from .forms import Login_form,Registration_form
 
 def homepage(request):
     if request.method=="GET":
-        form = Login_form()
-        context={"title":"Musify","login_form":form}
+        login_form = Login_form()
+        registration_form = Registration_form()
+        context={"title":"Musify","login_form":login_form,"registration_form":registration_form}
         return render(request,"persons/index.html",context)
     else:
         if request.POST.get('email'):
-            print 'signup form'
+            registration_form=Registration_form(request.POST)
+            if registration_form.is_valid():
+                instance=registration_form.save(commit=False)
+                password=registration_form.cleaned_data.get('passsword')
+                instance.set_password(password)
+                instance.save()
+            context = {"title": "Musify","registration_form":registration_form,"login_form":Login_form()}
+            return render(request, "persons/index.html", context)
         else:
-            form=Login_form(request.POST)
-            if form.is_valid():
-                username=form.cleaned_data.get('username')
-                password=form.cleaned_data.get('password')
-            context = {"title": "Musify", "login_form": form}
+            login_form=Login_form(request.POST)
+            if login_form.is_valid():
+                username=login_form.cleaned_data.get('username')
+                password=login_form.cleaned_data.get('password')
+            context = {"title": "Musify","registratio_form":Registration_form(),"login_form":login_form}
             return render(request, "persons/index.html", context)
